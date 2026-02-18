c0f02d510ce149d7ef639f06c35074411eaf8277
Monty <montyjangid291@gmail.com>
2026-02-18 21:25:55 +0530

    stock sync update

--- server.js
+++ server.js
@@ -1,4 +1,15 @@
 const express = require("express");
+const app = express();
+// ===== GLOBAL STORE STOCK (ONLINE SAME FOR EVERY DEVICE) =====
+let storeStock = {
+Maggi:24,
+Kurkure:9,
+Bhujia:2,
+Ariel:1,
+"Bhoot Chips":5,
+"Bingo Onion Chips":5
+};
+
 const cors = require("cors");
 const path = require("path");
 
@@ -11,9 +22,28 @@ let orders = [];
 /* ---- HTML serve ---- */
 app.use(express.static(__dirname));
 
+// ===== GET STOCK =====
+app.get("/stock",(req,res)=>{
+res.json(storeStock);
+});
+
+// ===== UPDATE STOCK BY ADMIN =====
+app.post("/stock",(req,res)=>{
+storeStock=req.body;
+console.log("Stock Updated:",storeStock);
+res.json({status:"saved"});
+});
+
 /* ---- receive order ---- */
 app.post("/order",(req,res)=>{
 const order=req.body;
+
+order.items.forEach(i=>{
+if(storeStock[i.name]!==undefined){
+storeStock[i.name]-=i.qty;
+}
+});
+
 
 order.id=Date.now();
 order.time=new Date().toLocaleString();
